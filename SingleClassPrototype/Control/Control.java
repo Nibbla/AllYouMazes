5856f2d5b5635ca3b2b2751ac5e5afd9457a8723
Eric Dallmeier <e.dallmeier@student.maastrichtuniversity.nl>
2017-10-11 19:24:56 +0200

    initial commit, this code works under Ubuntu 14.04
    When using this code as separate Code, i.e. removing all 'Overrides' and references to outside classes and making use of a separate Main class, an instance of Control can be created, started and tested. It does then connect to the specified ePuck and executes the commands listed.
    
    To get this code to work certain requirements are necessary:
    1) Perform a Desktop-Full installation of ROS 'Indigo' (http://wiki.ros.org/indigo/Installation/Ubuntu)
    
    2) Go through all the steps mentioned in Chapter 3.1 'Initial configuration' of the following document (http://www.gctronic.com/doc/index.php/E-Puck#ROS). Note that instead of step 3 and 4, downloading the epuck_driver from github, I advise to use the epuck_driver folder provided under "additional Files", in the folder 'src' in the archive catkin_ws.zip, as I made some small changes to it.
    
    3) Set the epuck-selector to mode 3.
    
    4) Pair the epuck via bluetooth. The requested PIN is equal to the four-digit number on the epuck.
    
    5) After pairing with the epuck it's MAC-Address can be obtained. Add it in the document ~/catkin_ws/src/epuck_driver/multi_epuck.launch where it belongs.
    
    6) Execute the code.
    
    The class was previously designed to have an getInstance(), probably to be able to retrieve always the same Instance from different places without having to pass the one-time-created Instance around. As my desgin-patterns are a bit rusty I did not implement this for now, but I guess changing some of the fields would easily resolve this issue. Also I think that there should not be that many different places that need an Instace of this class, so maybe we don't need the getInstance()?

--- SingleClassPrototype/Control/Control.java
+++ SingleClassPrototype/Control/Control.java
@@ -4,35 +4,145 @@ import Interfaces.IControl;
 import Model.Path;
 import SpecialSettingsEtc.Tangential;
 
+import java.util.Map;
+
 /**
  * Created by Nibbla on 27.09.2017.
  */
 public class Control implements IControl {
-    private static Control factoryControl = new Control();
-    public Control getInstance() throws CloneNotSupportedException {
-        return (Control) factoryControl.clone();
+
+    private final String ROSversion = "indigo";
+    private final String username = "eric";
+    private final int startUpSeconds = 20;
+
+    private final String[] startCommand = {"bash", "-c", "/opt/ros/" + ROSversion + "/bin/roslaunch -p 11311 -v --screen epuck_driver multi_epuck.launch"};
+    private final String[] movementCommand = {"bash", "-c", "/opt/ros/" + ROSversion + "/bin/rostopic pub --once /epuck_robot_0/mobile_base/cmd_vel geometry_msgs/Twist \'{linear:  {x: 0, y: 0.0, z: 0.0}, angular: {x: 0.0, y: 0.0,z: 0.0}}\'"};
+
+    private ProcessBuilder processGenerator;
+    private Process initRosProcess;
+    private Process motorSpeedProcess;
+
+
+    public Control() {
+        this.initProcessBuilder();
     }
 
 
+    @Override
+    public void startConnection() {
+        this.initConnection();
+    }
 
+    @Override
+    public void resetConnection() {
+        this.initRosProcess.destroy();
+        this.initConnection();
+    }
+
+    @Override
+    public void closeConnection() {
+
+        this.initRosProcess.destroy();
+    }
 
     @Override
     public boolean setPath(Path pathway) {
+
         return false;
     }
 
     @Override
     public boolean move(Path pathway) {
+
         return false;
     }
 
     @Override
     public boolean move(double[] moveDouble) {
+
         return false;
     }
 
     @Override
     public boolean move(Tangential.Direction Direction) {
+
         return false;
+    }
+
+    public void testCommands() throws InterruptedException {
+        this.moveStraight(1);
+        Thread.sleep(3000);
+        this.stop();
+        Thread.sleep(3000);
+        this.rotate(1);
+        Thread.sleep(3000);
+        this.stop();
+    }
+
+    private void initProcessBuilder() {
+        processGenerator = new ProcessBuilder();
+
+        processGenerator.redirectOutput(ProcessBuilder.Redirect.INHERIT);
+
+        Map<String, String> env = processGenerator.environment();
+
+        env.put("LD_LIBRARY_PATH", "/home/" + username + "/catkin_ws/devel/lib:/opt/ros/" + ROSversion + "/lib");
+        env.put("CPATH", "/opt/ros/" + ROSversion + "/include");
+        env.put("PATH", env.get("PATH") + ":/opt/ros/" + ROSversion + "/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/home/" + username + "/bin:/usr/lib/jvm/jdk1.8.0/bin:/opt/microchip/xc16/v1.32/bin:/opt/microchip/xc16/v1.32/bin");
+        env.put("ROSLISP_PACKAGE_DIRECTORIES", "/home/" + username + "/catkin_ws/devel/share/common-lisp");
+        env.put("PYTHONPATH", "/home/" + username + "/catkin_ws/devel/lib/python2.7/dist-packages:/opt/ros/" + ROSversion + "/lib/python2.7/dist-packages");
+        env.put("ROS_DISTRO", ROSversion);
+        env.put("ROS_ROOT", "/opt/ros/" + ROSversion + "/share/ros");
+        env.put("ROS_PACKAGE_PATH", "/home/" + username + "/catkin_ws/src:/opt/ros/" + ROSversion + "/share:/opt/ros/" + ROSversion + "/stacks");
+        env.put("ROS_MASTER_URI", "http://localhost:11311");
+        env.put("PKG_CONFIG_PATH", "/home/" + username + "/catkin_ws/devel/lib/pkgconfig:/opt/ros/" + ROSversion + "/lib/pkgconfig");
+        env.put("CMAKE_PREFIX_PATH", "/home/" + username + "/catkin_ws/devel:/opt/ros/" + ROSversion);
+        env.put("ROS_ETC_DIR", "/opt/ros/" + ROSversion + "/etc/ros");
+        env.put("PWD", "/home/" + username);
+    }
+
+    private void initConnection() {
+        try {
+            processGenerator.command(startCommand);
+
+            initRosProcess = processGenerator.start();
+
+            Thread.sleep(startUpSeconds * 1000);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void issueMotorSpeed() {
+        try {
+            processGenerator.command(movementCommand);
+            motorSpeedProcess = processGenerator.start();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void setMotorSpeed(double linear, double angular) {
+        movementCommand[2] = "/opt/ros/indigo/bin/rostopic pub --once /epuck_robot_0/mobile_base/cmd_vel geometry_msgs/Twist \'{linear:  {x: " + linear + ", y: 0.0, z: 0.0}, angular: {x: 0.0, y: 0.0,z: " + angular + "}}\'";
+    }
+
+    private void moveCurve(double linear, double angular) {
+        setMotorSpeed(linear, angular);
+        issueMotorSpeed();
+    }
+
+    private void moveStraight(double speed) {
+        setMotorSpeed(speed, 0);
+        issueMotorSpeed();
+    }
+
+    private void rotate(double speed) {
+        setMotorSpeed(0, speed);
+        issueMotorSpeed();
+    }
+
+    private void stop() {
+        setMotorSpeed(0, 0);
+        issueMotorSpeed();
     }
 }
