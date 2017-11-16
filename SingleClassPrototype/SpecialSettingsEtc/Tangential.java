b3ceceff3ff385be0f7550a305a80533791ddc37
Eric Dallmeier <e.dallmeier@student.maastrichtuniversity.nl>
2017-11-16 10:54:04 +0100

    - adjusted tangential directions(x,y) s.t. x and y do NOT represent the motorspeed for the left and right motor respectively but x represents the forward speed and y represents the rotationalspeed as implemented in the RoboControl for that also ajusted namefields to be more fitting

--- SingleClassPrototype/SpecialSettingsEtc/Tangential.java
+++ SingleClassPrototype/SpecialSettingsEtc/Tangential.java
@@ -1,8 +1,8 @@
 package SpecialSettingsEtc;
 
 public class Tangential {
-    private final static float[] sins = new float[2^8];
-    private final static float[] coss = new float[2^8];
+    private final static float[] sins = new float[2 ^ 8];
+    private final static float[] coss = new float[2 ^ 8];
 
     public static float sin(byte theta) {
         return sins[theta];
@@ -12,13 +12,16 @@ public class Tangential {
         return coss[phi];
     }
 
-    public enum Direction {forward(1.,0),backward(-1.,0),turnLeftForward(1.,1.),turnRightForward(1.,-1.),turnLeftBackward(-1,1),TurnRightBackward(-1.,-1),RotateLeft(0,1.),RotateRight(0,-1.);
+    public enum Direction {
+        forward(1., 0), backward(-1., 0), turnLeftForward(1., 1.), turnRightForward(1., -1.), turnLeftBackward(-1, 1), TurnRightBackward(-1., -1), RotateLeft(0, 1.), RotateRight(0, -1.);
 
 
-        public final double[] powerLevel;
+        public final double linearSpeed, angularSpeed;
 
         Direction(double v, double v1) {
-            this.powerLevel = new double[]{v,v1};
+            this.linearSpeed = v;
+            this.angularSpeed = v1;
         }
     }
 }
+
